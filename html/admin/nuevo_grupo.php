81d77488b68e5c1abfdc0ebac290af09ee8d6553
Félix Arreola Rodríguez <fgatuno.123@gmail.com>
2011-09-14 22:00:32 -0500

    Agrego Jquery para ocultar partes del documento
    * Correciones menores para el validador W3c

--- html/admin/nuevo_grupo.php
+++ html/admin/nuevo_grupo.php
@@ -24,15 +24,48 @@
 	require_once '../global-config.php'; # Debería ser Require 'global-config.php'
 	echo $cfg['nombre'];
 	?></title>
-	
+	<style type="text/css">
+	.oculto {	
+		display:none;
+	}
+	</style>
+	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
 	<script language="javascript" type="text/javascript">
+		function ocultar_depas () {
+			var j_depa = document.getElementsByName("depas");
+			
+			for (g = 0; g < j_depa.length; g++) {
+				if (j_depa[g].checked) {
+					if (g == 0) {
+						/* La primera casilla de verificación, sin depas */
+						$("#j_depa_1").slideUp("fast");
+						$("#j_depa_2").slideUp("fast");
+					} else if (g == 1) {
+						$("#j_depa_1").slideDown("fast");
+						$("#j_depa_2").slideUp("fast");
+					} else if (g == 2) {
+						$("#j_depa_1").slideDown("fast");
+						$("#j_depa_2").slideDown("fast");
+					}
+				}
+			}
+		}
 		
+		function ocultar_puntos () {
+			if (document.getElementById ("puntos").checked) {
+				$("#j_puntos").stop (false, true);
+				$("#j_puntos").slideDown("fast");
+			} else {
+				$("#j_puntos").stop (false, true);
+				$("#j_puntos").slideUp("fast");
+			}
+		}
 	</script>
 </head>
 <body>
 	<h1>Agregar una nueva sección</h1>
-	<form action="" method="POST">
-	<p>Nrc:<input name="nrc" id="nrc" type="text" length="5" /></p>
+	<form action="" method="post">
+	<p>Nrc:<input name="nrc" id="nrc" type="text" /></p>
 	<?php
 		require_once "../mysql-con.php";
 		/* Listar todas las materias */
@@ -49,7 +82,7 @@
 		mysql_free_result ($result);
 		
 		echo "</select></p>\n";
-		echo "<p>Sección:<input name=\"seccion\" id=\"seccion\" type=\"text\" length=\"3\" /></p>\n";
+		echo "<p>Sección:<input name=\"seccion\" id=\"seccion\" type=\"text\" /></p>\n";
 		echo "<p>Maestro:<select name=\"maestro\" id=\"maestro\">\n";
 		
 		$query = "SELECT Codigo, Nombre, Apellido FROM Maestros";
@@ -66,21 +99,21 @@
 		
 		mysql_close ($mysql_con);
 	?>
-	<p><input type="radio" name="depas" id="depas" checked="checked" value="0" />La materia no tiene departamentales<br />
-	<input type="radio" name="depas" id="depas" value="1" />La materia tiene un departamental<br />
-	<input type="radio" name="depas" id="depas" value="2" />La materia tiene dos departamentales</p>
+	<p><input type="radio" name="depas" checked="checked" value="0" onchange="ocultar_depas ()" />La materia no tiene departamentales<br />
+	<input type="radio" name="depas" value="1" onchange="ocultar_depas ()" />La materia tiene un departamental<br />
+	<input type="radio" name="depas" value="2" onchange="ocultar_depas ()" />La materia tiene dos departamentales</p>
 	
-	<span id="j_puntos_depa1" style="visibility:hidden">Porcentaje del primer departamental:
+	<div class="oculto" id="j_depa_1">Porcentaje del primer departamental:
 	<input name="puntos_depa1" id="puntos_depa1" type="text" />
-	</span>
-	<span id="j_puntos_depa2" style="visibility:hidden"><br />Porcentaje del segundo departamental: 
+	</div>
+	<div class="oculto" id="j_depa_2">Porcentaje del segundo departamental: 
 	<input name="puntos_depa2" id="puntos_depa2" type="text" />
-	</span>
+	</div>
 	
-	<p><input name="puntos" id="puntos" type="checkbox" value="D" onchange="ocultar_puntos_maestro ()" />El maestro asigna puntos en esta materia</p>
-	<span id="j_puntos" style="visibility:hidden">Porcentaje de los punto asignados por el maestro:
+	<p><input name="puntos" id="puntos" type="checkbox" value="1" onchange="ocultar_puntos ()" />El maestro asigna puntos en esta materia</p>
+	<div class="oculto" id="j_puntos">Porcentaje de los punto asignados por el maestro:
 	<input name="n_puntos" id="n_puntos" type="text" />
-	</span>
+	</div>
 	
 	</form>
 </body>
