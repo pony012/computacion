564fcfc08ba26dff06234a874d7cefbc60c0939d
Félix Arreola Rodríguez <fgatuno.123@gmail.com>
2011-10-16 15:39:53 -0500

    Código para eliminar una sección

---
+++ html/admin/eliminar_seccion.php
@@ -0,0 +1,59 @@
+<?php
+	session_start ();
+	
+	/* Primero verificar una sesión válida */
+	if (!isset ($_SESSION['auth']) || $_SESSION['auth'] != 1) {
+		/* Tenemos un intento de acceso inválido */
+		header ("Location: login.php");
+		exit;
+	}
+	
+	/* Luego verificar si tiene el permiso de crear grupos */
+	if (!isset ($_SESSION['permisos']['crear_grupos']) || $_SESSION['permisos']['crear_grupos'] != 1) {
+		/* Privilegios insuficientes */
+		header ("Location: vistas.php");
+		exit;
+	}
+	
+	if (!isset ($_GET['nrc'])) {
+		header ("Location: secciones.php?r=null");
+		exit;
+	}
+	
+	if (!isset ($_GET['confirmado_js']) || $_GET['confirmado_js'] != 1) {
+		header ("Location: secciones.php?r=err");
+		exit;
+	}
+	
+	/* Validar primero el NRC */
+	if (!preg_match ("/^([0-9]){5}$/", $_GET['nrc'])) {
+		header ("Location: secciones.php?e=nrc");
+		exit;
+	}
+	
+	require_once '../mysql-con.php';
+	
+	/* Impedir que eliminen la sección si tiene alumnos */
+	$query = sprintf ("SELECT * FROM Grupos WHERE Nrc='%s'", $_GET['nrc']);
+	
+	$result = mysql_query ($query, $mysql_con);
+	
+	if (mysql_num_rows($result) == 0) {
+		header ("Location: secciones.php?r=uso");
+		exit;
+	}
+	
+	mysql_free_result ($result);
+	
+	$query = sprintf ("DELETE FROM Secciones WHERE Nrc='%s'", $_GET['nrc']);
+	
+	$result = mysql_query ($query, $mysql_con);
+	
+	if ($result) {
+		header ("Location: secciones.php?r=ok");
+	} else {
+		header ("Location: secciones.php?r=no");
+	}
+	
+	exit;
+?>
