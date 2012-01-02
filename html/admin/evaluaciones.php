45f9ead202ce7c9d231297ef0cfa3771217ed487
Félix Arreola Rodríguez <fgatuno.123@gmail.com>
2012-01-02 13:33:39 -0600

    Elimino paginacion de la lista de evaluaciones

--- html/admin/evaluaciones.php
+++ html/admin/evaluaciones.php
@@ -32,70 +32,33 @@
 	<?php
 		require_once "../mysql-con.php";
 		
-		/* Recuperar la cantidad total de filas */
-		$result = mysql_query ("SELECT COUNT(*) AS TOTAL FROM Evaluaciones", $mysql_con);
-		
-		$row = mysql_fetch_object ($result);
-		$total = $row->TOTAL;
-		mysql_free_result ($result);
-		
-		$offset = (isset ($_GET['off'])) ? $_GET['off'] : 0;
-		settype ($offset, "integer");
-		$cant = 50;
-		$show = $cant;
-		
-		if ($offset >= $total) $offset = $total - $cant;
-		if ($offset < 0) $offset = 0;
-		if (($offset + $cant) >= $total) $show = $total - $offset;
-		
-		echo "<p>Mostrando del ". ($offset + 1) ." al ". ($offset + $show) . "</p>";
-		
 		echo "<table border=\"1\">";
 		
 		/* Mostrar la cabecera */
 		echo "<thead><tr><th>Nombre</th><th colspan=\"2\">Acción</th></tr></thead>";
 		
 		/* Empezar la consulta mysql */
-		$query = "SELECT * FROM Evaluaciones LIMIT ". $offset . ",". $show;
+		$query = "SELECT * FROM Evaluaciones";
 		
 		$result = mysql_query ($query, $mysql_con);
 		
 		echo "<tbody>";
 		while (($object = mysql_fetch_object ($result))) {
 			echo "<tr>";
-			/* El nrc */
+			
 			printf ("<td>%s<td>", $object->Descripcion);
 			if ($object->Id != 0) { /* El extraordinario es un caso especial */
-				printf ("<a href=\"edit_eval.php?m=e&id=%s\"><img class=\"icon\" src=\"../img/properties.png\" /></a>", $object->Id);
+				printf ("<td><a href=\"edit_eval.php?m=e&id=%s\"><img class=\"icon\" src=\"../img/properties.png\" /></a>", $object->Id);
 				
 				printf ("<a href=\"eliminar_eval.php?id=%s\"\n", $object->Id);
 				printf (" onclick=\"return confirmarDrop(this, '¿Realmente desea eliminar %s?')\">", $object->Descripcion);
-				printf ("<img class=\"icon\" src=\"../img/remove.png\" /></a>");
+				printf ("<img class=\"icon\" src=\"../img/remove.png\" /></a></td>");
 			}
 			
 			echo "</tr>\n";
 		}
 		
 		echo "</tbody></table>\n";
-		echo "<p>";
-		
-		$next = $offset + $show;
-		$ultimo = $total - ($total % $cant);
-		if ($next >= $total) $next = $ultimo;
-		$prev = $offset - $cant;
-		if ($prev < 0) $prev = 0;
-		
-		/* Mostrar las flechas de dezplamiento */
-		if ($offset > 0) {
-			printf ("<a href=\"%s?off=0\"><img class=\"icon\" src=\"../img/first.png\" /></a>\n", $_SERVER['SCRIPT_NAME']);
-			printf ("<a href=\"%s?off=%s\"><img class=\"icon\" src=\"../img/prev.png\" /></a>\n", $_SERVER['SCRIPT_NAME'], $prev);
-		}
-		if ($offset + $show < $total) { 
-			printf ("<a href=\"%s?off=%s\"><img class=\"icon\" src=\"../img/next.png\" /></a>\n", $_SERVER['SCRIPT_NAME'], $next);
-			printf ("<a href=\"%s?off=%s\"><img class=\"icon\" src=\"../img/last.png\" /></a>\n", $_SERVER['SCRIPT_NAME'], $ultimo);
-		}
-		
-		echo "</p>\n";
 	?>
 	<ul><li><a href="edit_eval.php?m=n">Nueva forma de evaluación</a></li></ul>
 </body>
