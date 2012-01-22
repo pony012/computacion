bced37fbb0cba2cc543352b6213c4c706c4c7f8d
Félix Arreola Rodríguez <fgatuno.123@gmail.com>
2012-01-21 18:26:38 -0600

    Reparo auto_aplicadores.

--- html/admin/post_auto_salones.php
+++ html/admin/post_auto_salones.php
@@ -25,11 +25,10 @@
 	}
 	
 	settype ($_POST['fecha'], 'integer');
+	$tiempo_fecha = $_POST['fecha'] - ($_POST['fecha'] % 900);
 	settype ($_POST['evaluacion'], 'integer');
 	
 	require_once '../mysql-con.php';
-	
-	$tiempo_fecha = $_POST['fecha'] - ($_POST['fecha'] % 900);
 	
 	/* Validar que exista la forma de evaluacion seleccionada con la materia seleccionada */
 	/* SELECT * FROM Porcentajes AS P INNER JOIN Evaluaciones AS E ON P.Tipo = E.Id INNER JOIN Materias AS M ON P.Clave = M.Clave */
@@ -71,40 +70,48 @@
 			exit;
 		}
 		/* SELECT G.Alumno FROM Grupos AS G INNER JOIN Secciones AS S ON G.Nrc = S.Nrc INNER JOIN Alumnos AS A ON G.Alumno = A.Codigo WHERE S.Materia = 'CC100' ORDER BY A.Apellido, A.Nombre */
-		$query = sprintf ("SELECT G.Alumno FROM Grupos AS G INNER JOIN Secciones AS S ON G.Nrc = S.Nrc INNER JOIN Alumnos AS A ON G.Alumno = A.Codigo WHERE S.Materia = '%s' ", $_POST['materia']);
+		$query = sprintf ("SELECT G.Alumno FROM Grupos AS G INNER JOIN Secciones AS S ON G.Nrc = S.Nrc INNER JOIN Alumnos AS A ON G.Alumno = A.Codigo WHERE S.Materia = '%s'", $_POST['materia']);
 		
 		if ($_POST['select_order'] == 'order') {
-			$query = $query . "ORDER BY A.Apellido, A.Nombre";
+			$query = $query . " ORDER BY A.Apellido, A.Nombre";
 		} else {
-			$query = $query . "ORDER BY RAND()";
+			$query = $query . " ORDER BY RAND()";
 		}
 		
 		$result = mysql_query ($query, $mysql_con);
 		
 		$g = 0;
 		$salon = 1;
-		$query_aplicadores = "INSERT INTO Aplicadores (Alumno, Materia, Tipo, Salon, FechaHora, Maestro) VALUES ";
+		$id_salon = -1;
+		$query_aplicadores = "INSERT INTO Alumnos_Aplicadores (Id, Alumno) VALUES";
 		
 		while (($object = mysql_fetch_object ($result))) {
 			$g++;
 			
-			/* Tomar un maestro de la lista */
-			if (isset ($_POST['maestro'][$salon - 1])) {
-				$un_maestro = $_POST['maestro'][$salon - 1];
-			} else {
-				$un_maestro = "NULL";
+			if ($id_salon == -1) {
+				/* Tomar un maestro de la lista */
+				if (isset ($_POST['maestro'][$salon - 1])) {
+					$un_maestro = $_POST['maestro'][$salon - 1];
+				} else {
+					$un_maestro = "NULL";
+				}
+				
+				$query = sprintf ("INSERT INTO Salones_Aplicadores (Materia, Tipo, Salon, FechaHora, Maestro) VALUES ('%s', '%s', 'Salon %s', FROM_UNIXTIME ('%s'), %s);", $_POST['materia'], $_POST['evaluacion'], $salon, $tiempo_fecha, $un_maestro);
+				
+				mysql_query ($query, $mysql_con);
+				
+				$id_salon = mysql_insert_id ($mysql_con);
 			}
 			
-			$query_aplicadores = $query_aplicadores . sprintf ("('%s', '%s', '%s', 'Salon %s', FROM_UNIXTIME ('%s'), %s),", $object->Alumno, $_POST['materia'], $_POST['evaluacion'], $salon, $tiempo_fecha, $un_maestro);
+			$query_aplicadores = $query_aplicadores . sprintf (" ('%s', '%s'),", $id_salon, $object->Alumno);
 			
 			if ($g == $_POST['no_alumnos']) {
-				/* Verificar que el maestro asignado existe */
-				
 				/* Ejecutar la query, resetar la consulta y aumentar el salón */
-				$query_aplicadores = substr_replace ($query_aplicadores, " ", -1) . " ON DUPLICATE KEY UPDATE Salon = VALUES(Salon), FechaHora = VALUES(FechaHora), Maestro = VALUES(Maestro);";
+				$query_aplicadores = substr_replace ($query_aplicadores, ";", -1);
 				mysql_query ($query_aplicadores, $mysql_con);
-				$query_aplicadores = "INSERT INTO Aplicadores (Alumno, Materia, Tipo, Salon, FechaHora, Maestro) VALUES ";
+				$query_aplicadores = "INSERT INTO Alumnos_Aplicadores (Id, Alumno) VALUES";
 				$salon++;
+				$id_salon = -1;
 				$g = 0;
 			}
 		}
@@ -112,7 +119,7 @@
 		mysql_free_result ($result);
 		
 		if ($g != 0) {
-			$query_aplicadores = substr_replace ($query_aplicadores, " ", -1) . " ON DUPLICATE KEY UPDATE Salon = VALUES(Salon), FechaHora = VALUES(FechaHora), Maestro = VALUES(Maestro);";
+			$query_aplicadores = substr_replace ($query_aplicadores, ";", -1);
 			mysql_query ($query_aplicadores, $mysql_con);
 		}
 		
@@ -126,15 +133,20 @@
 		$salon = 1;
 		while (($nrc = mysql_fetch_object ($result))) {
 			/* Por cada Nrc, generar un query extendido */
-			$query_aplicadores = "INSERT INTO Aplicadores (Alumno, Materia, Tipo, Salon, FechaHora, Maestro) VALUES ";
+			$query = sprintf ("INSERT INTO Salones_Aplicadores (Materia, Tipo, Salon, FechaHora, Maestro) VALUES ('%s', '%s', 'Salon %s', FROM_UNIXTIME ('%s'), '%s');", $_POST['materia'], $_POST['evaluacion'], $salon, $tiempo_fecha, $nrc->Maestro);
 			
+			mysql_query ($query, $mysql_con);
+			$id_salon = mysql_insert_id ($mysql_con);
+			
+			$query_aplicadores = "INSERT INTO Salones_Alumnos (Id, Alumno) VALUES";
 			$query = sprintf ("SELECT Alumno FROM Grupos WHERE Nrc = '%s'", $nrc->Nrc);
 			
 			$res_al = mysql_query ($query);
 			
 			while (($object = mysql_fetch_object ($res_al))) {
-				$query_aplicadores = $query_aplicadores . sprintf ("('%s', '%s', '%s', 'Salon %s', FROM_UNIXTIME ('%s'), '%s'),", $object->Alumno, $_POST['materia'], $_POST['evaluacion'], $salon, $tiempo_fecha, $nrc->Maestro);
+				$query_aplicadores = $query_aplicadores . sprintf (" ('%s', '%s'),", $id_salon, $object->Alumno);
 			}
+			
 			mysql_free_result ($res_al);
 			
 			$query_aplicadores = substr_replace ($query_aplicadores, ";", -1);
